c010d12d6148435c1ce260887bf5d91fcaaf1919
Nerzull3 <dima3.98@mail.ru>
2018-11-05 20:09:33 +0500

    1st attempt without extra

--- emitter.js
+++ emitter.js
@@ -4,13 +4,45 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const subscribeData = {};
+
+    function getChildrenEvents(event) {
+        return Object.keys(subscribeData).filter(currentEvent =>
+            currentEvent.startsWith(event + '.')
+        );
+    }
+
+    function addSubscribe(event, subscribe) {
+        if (subscribeData[event]) {
+            subscribeData[event].push(subscribe);
+        } else {
+            subscribeData[event] = [subscribe];
+        }
+    }
+
+    function tryToCallHandler(person) {
+        if (!person.counter && !person.frequency ||
+            checkCounterProperty(person) ||
+            checkFrequencyProperty(person)) {
+            person.handler.call(person.name);
+        }
+    }
+
+    function checkCounterProperty(person) {
+        return person.counter && person.counter-- !== 0;
+    }
+
+    function checkFrequencyProperty(person) {
+        return person.frequency && person.eventCounter++ % person.frequency === 0;
+    }
+
     return {
 
         /**
@@ -18,26 +50,44 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            addSubscribe(event, { name: context, handler: handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            [event].concat(getChildrenEvents(event)).forEach(currentEvent => {
+                subscribeData[currentEvent] =
+                    subscribeData[currentEvent].filter(person => person.name !== context);
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            if (Object.keys(subscribeData).includes(event)) {
+                subscribeData[event].forEach(person => tryToCallHandler(person));
+            }
+            let eventParent = event.replace(/\.[\w.]+$/, '');
+            if (eventParent !== event) {
+                this.emit(eventParent);
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +97,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            addSubscribe(event, { name: context, handler: handler, counter: times });
+
+            return this;
         },
 
         /**
@@ -59,9 +112,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            addSubscribe(
+                event,
+                { name: context, handler: handler, frequency: frequency, eventCounter: 0 }
+            );
+
+            return this;
         }
     };
 }
